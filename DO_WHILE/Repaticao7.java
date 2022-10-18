608cccc8528f65408334e2c026fb92758aedd5be
Anakeylarodrigues <anakeylarodrigues1@gmail.com>
2022-10-17 22:34:24 -0300

    atualizando

---
+++ DO_WHILE/Repaticao7.java
@@ -0,0 +1,12 @@
+public class Repaticao7 {
+    
+    public static void main(String[] args) {
+        
+        int i = 10;
+
+        do {
+            System.out.println("Valor de i = " + i);
+            i+=10;
+        } while (i <= 50);
+    }
+}
