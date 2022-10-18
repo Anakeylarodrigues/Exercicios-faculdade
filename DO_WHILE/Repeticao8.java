0be6c1afdc7da81dd7114551b525fc2aa8f43b52
Anakeylarodrigues <anakeylarodrigues1@gmail.com>
2022-10-17 22:46:27 -0300

    atualizando

---
+++ DO_WHILE/Repeticao8.java
@@ -0,0 +1,16 @@
+public class Repeticao8 {
+
+    /*A variável identificada por i, receberá, inicialmente, o valor 1, 
+os comandos escreval (“valor de i = “,i) e i <- i + 1 serão executados até a condição 
+se tornar verdadeira, ou seja, até i > 5 */
+    
+    public static void main(String[] args) {
+        
+        int i = 1;
+
+        do {
+            System.out.println("Valor de i = " + i);
+            i+=1;
+        } while (i <= 5);
+    }
+}
