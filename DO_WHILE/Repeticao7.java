3473ef50f53b9ab6cc9c6d4b17215c9e24b96855
Anakeylarodrigues <anakeylarodrigues1@gmail.com>
2022-10-19 16:24:43 -0300

    atualizando

--- DO_WHILE/Repeticao7.java
+++ DO_WHILE/Repeticao7.java
@@ -2,7 +2,7 @@
 10, os comandos escreval (“valor de i = ”,i) e i ← i + 10 serão executados até a 
 condição se tornar verdadeira, ou seja, até i > 50 */
 
-public class Repaticao7 {
+public class Repeticao7 {
     
     public static void main(String[] args) {
         
